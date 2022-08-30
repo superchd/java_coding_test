185dff39c0d8fda7cb4963913c464dc152a6afe0
hyundae <63406434+superchd@users.noreply.github.com>
2022-08-30 17:10:29 +0900

    run time error

--- dfs/basic.java
+++ dfs/basic.java
@@ -4,28 +4,29 @@ import java.util.ArrayList;
 public class Main {
     public static int VERTICES_NUM;
     public static int EDGES_NUM;
-    public static ArrayList<Integer>[] graph = new ArrayList[VERTICES_NUM + 1];
-    public static boolean[] visited = new boolean[VERTICES_NUM + 1];
-    
+    public static ArrayList<Integer>[] graph = new ArrayList[1001];
+    public static boolean[] visited = new boolean[1001];
+    public static int v_cnt = 0;
     public static void dfs(int vertex){
-        for (int i = 0; i < graph[i].size(); i++){
+        for (int i = 0; i < graph[vertex].size(); i++){
             int curr = graph[vertex].get(i);
             if (!visited[curr]){
                 visited[curr] = true;
-                System.out.println(curr);
+               
+                v_cnt++;
                 dfs(curr);
             }
         }
     }
 
     public static void main(String[] args) {
-        for (int i = 1; i<= VERTICES_NUM + 1; i++){
-            graph[i] = new ArrayList<>();
-        }
+       
         Scanner sc = new Scanner(System.in);
         VERTICES_NUM = sc.nextInt();
         EDGES_NUM = sc.nextInt();
-        
+        for (int i = 1; i<= VERTICES_NUM + 1; i++){
+            graph[i] = new ArrayList<>();
+        }
 
         for (int i = 0; i < EDGES_NUM; i++){
             int start = sc.nextInt();
@@ -35,7 +36,8 @@ public class Main {
         }
         int rootVertex = 1;
         visited[rootVertex] = true;
-        System.out.println(rootVertex);
+        
         dfs(rootVertex);
+        System.out.print(v_cnt);
     }
 }
